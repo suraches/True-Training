a6f120a2e87494f020983196a1ecaebb81960d95
Tee <admin@jquerytips.com>
2013-03-16 15:17:59 +0700

    layout

--- application/controllers/themes/layout.php
+++ application/controllers/themes/layout.php
@@ -2,6 +2,9 @@
 
 class Themes_Layout_Controller extends Base_Controller {
 
+	// Path: appliaction/views/layouts/default.php
+	public $layout = 'layouts.default';
+
 	public function get_index()
 	{
 
