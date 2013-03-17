978262f0d5e2ead8831c4a6d9d51e21eb9e5d81d
Tee <admin@jquerytips.com>
2013-03-17 11:24:51 +0700

    methods

--- application/controllers/user/register.php
+++ application/controllers/user/register.php
@@ -2,12 +2,38 @@
 
 class User_Register_Controller extends Base_Controller {
 
+	public function get_index()
+	{
+
+	}
+
 	public function get_create()
 	{
-		return View::of('default');
+		return View::of('default')
+			->nest('content','user.register.create');
 	}
 
 	public function post_create()
+	{
+
+	}
+
+	public function get_edit()
+	{
+
+	}
+
+	public function post_edit()
+	{
+
+	}
+
+	public function get_delete()
+	{
+
+	}
+
+	public function post_delete()
 	{
 
 	}
